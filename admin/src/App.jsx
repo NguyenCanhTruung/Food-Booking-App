dab240e4c9c89db2e4cd6fb5e6fbbd265e0daeb4
NguyenCanhTruung <Trung.nc220121@sis.hust.edu.vn>
2025-06-10 07:01:40 +0700

    deploy web

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://food-booking-app-y7yx.onrender.com/";
 
   return (
     <div>
